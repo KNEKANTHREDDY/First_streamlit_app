470723656f3322a50aade654011afaf03f782629
KNEKANTHREDDY <123460633+KNEKANTHREDDY@users.noreply.github.com>
2023-01-24 18:02:00 +0530

    Update Streamlit_app.py
    Added Breakfast Ment Header And 3items

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.title('Breakfast menu')
 streamlit.title('omega3 and oate meal')
 streamlit.title('kale , spinach Rocket smoothie')
 streamlit.title('Hard-Boiled egg, free-range egg')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
