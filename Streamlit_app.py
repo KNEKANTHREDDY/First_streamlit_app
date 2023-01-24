35474a1f2bccfe78c6ed5367b713300b575a2eae
KNEKANTHREDDY <123460633+KNEKANTHREDDY@users.noreply.github.com>
2023-01-24 18:21:41 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,4 +12,6 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado and Toast ')
 import pandas
-
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
+my_fruit_list = pandas.read_csv("https://https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
