a92c7e282cb0b714225bd34c4ed4217d99279e69
KNEKANTHREDDY <123460633+KNEKANTHREDDY@users.noreply.github.com>
2023-01-27 16:35:09 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,16 +1,1 @@
-import streamlit
-streamlit.title('My parents healthy diner')
-
-streamlit.title('Breakfast menu')
-streamlit.title('omega3 and oate meal')
-streamlit.title('kale , spinach Rocket smoothie')
-streamlit.title('Hard-Boiled egg, free-range egg')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avacado and Toast ')
-streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
-import pandas
 
